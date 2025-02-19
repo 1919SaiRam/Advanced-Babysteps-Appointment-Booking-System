656f9d90c23fd067375b02bd2fcd5c8fe4de5a36
unknown <vijaykumarchinta1919@gmail.com>
2025-02-19 21:56:08 +0530

    main

---
+++ frontend/src/pages/DoctorPage.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { useparams} from "react-router-dom";
+import SlotPicker from "../components/SlotPicker";
+import AppointmentForm from "../components/AppointmentForm";
+
+const DoctorPage = () => {
+
+}
+export default DoctorPage;
